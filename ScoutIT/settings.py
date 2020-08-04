8312fc866ad8b034c66de97ec4aa18a3700eab7b
yassine-eluharani <eluharani.yassine@gmail.com>
2020-08-04 21:22:05 +0100

    shit loads of icons and js

--- ScoutIT/settings.py
+++ ScoutIT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gq4!x+-0&mbz&w_c19tsk3e+0ga3cf39=42_hi6-2gydi_bi++'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['scout-it.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['scout-it.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -47,9 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
