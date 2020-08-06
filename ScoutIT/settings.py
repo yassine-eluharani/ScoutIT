860ab58efa1ed03b82b26f07800691fe4b2830aa
yassine-eluharani <eluharani.yassine@gmail.com>
2020-08-06 19:21:43 +0100

    Changed ALLOWED HOSTS to *

--- ScoutIT/settings.py
+++ ScoutIT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gq4!x+-0&mbz&w_c19tsk3e+0ga3cf39=42_hi6-2gydi_bi++'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['scout-it.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
