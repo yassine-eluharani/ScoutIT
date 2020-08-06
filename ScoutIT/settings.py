b094050f7848df10e2e7abef47fba373da6d45d4
yassine-eluharani <eluharani.yassine@gmail.com>
2020-08-06 20:24:48 +0100

    Still with server 500

--- ScoutIT/settings.py
+++ ScoutIT/settings.py
@@ -138,3 +138,20 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR , 'static/images')
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+
