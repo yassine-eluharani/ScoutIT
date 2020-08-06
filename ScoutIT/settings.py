41153efbbcf609c0ffad7454697c6c82e3517e4f
yassine-eluharani <eluharani.yassine@gmail.com>
2020-08-06 20:45:17 +0100

    It works but pic not loaded

--- ScoutIT/settings.py
+++ ScoutIT/settings.py
@@ -138,20 +138,5 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR , 'static/images')
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
